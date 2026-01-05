a65ec32215a461b1e7b0fd7fca2ba53e9fa28294
Shaunak Pandit <41136147+shaunakpandit@users.noreply.github.com>
2026-01-04 19:02:15 -0500

    add desktop host

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,65 @@
+{ pkgs, config, ... }: {
+  imports = [
+    # Mostly system related configuration
+    ../../nixos/nvidia.nix # CHANGEME: Remove this line if you don't have an Nvidia GPU
+    ../../nixos/audio.nix
+    ../../nixos/bluetooth.nix
+    ../../nixos/fonts.nix
+    ../../nixos/home-manager.nix
+    ../../nixos/nix.nix
+    ../../nixos/systemd-boot.nix
+    ../../nixos/sddm.nix
+    ../../nixos/users.nix
+    ../../nixos/utils.nix
+    ../../nixos/tailscale.nix
+    ../../nixos/hyprland.nix
+    ../../nixos/docker.nix
+
+    # ../../nixos/omen.nix # For my laptop only
+
+    # You should let those lines as is
+    ./hardware-configuration.nix
+    ./variables.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    # games
+    mangohud
+    protontricks
+  ];
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+  # for starCitizen
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };
+  # 1) udev rules for general input/hidraw + your VKB sticks
+  services.udev.extraRules = ''
+    # grant logind uaccess to every input device
+    SUBSYSTEM=="input"  , TAG+="uaccess"
+    SUBSYSTEM=="hidraw" , TAG+="uaccess"
+
+    # your existing VKB rule
+    KERNEL=="hidraw*", ATTRS{idVendor}=="231d", ATTRS{idProduct}=="*", MODE="0660", TAG+="uaccess"
+
+    # optional: always symlink your Gladiator to /dev/input/js0
+    KERNEL=="js[0-9]*", ATTRS{idVendor}=="231d", ATTRS{idProduct}=="*", \
+      SYMLINK+="input/js0", TAG+="uaccess"
+  '';
+
+  # 2) add your user to "input" so you can open /dev/input/*
+  users.users."${config.var.username}".extraGroups = [ "input" ];
+
+  # 3) ensure USB-HID modules are in the initrd for hot-plug
+  boot.kernelModules = [ "usbhid" "hid-generic" ];
+  home-manager.users."${config.var.username}" = import ./home.nix;
+
+  # enable udev rules for openRBG
+  services.hardware.openrgb.enable = true;
+
+  # Don't touch this
+  system.stateVersion = "24.05";
+}
---
+++ hosts/desktop/variables.nix
@@ -0,0 +1,36 @@
+{ config, lib, ... }: {
+  imports = [
+    # Choose your theme here:
+    ../../themes/darkmetal.nix
+  ];
+
+  config.var = {
+    hostname = "nexus";
+    username = "shaunak";
+    configDirectory = "/home/" + config.var.username
+      + "/.config/nixos"; # The path of the nixos configuration directory
+
+    keyboardLayout = "us";
+
+    location = "Washington DC";
+    timeZone = "America/New_York";
+    defaultLocale = "en_US.UTF-8";
+    extraLocale = "en_US.UTF-8";
+
+    git = {
+      username = "shaunakpandit";
+      email = "shaunakdpandit@gmail.com";
+    };
+
+    autoUpgrade = false;
+    autoGarbageCollector = true;
+  };
+
+  # Let this here
+  options = {
+    var = lib.mkOption {
+      type = lib.types.attrs;
+      default = { };
+    };
+  };
+}
