8966aa61a25c2fd885b4e6f103393674aa43d1c8
shaunakpandit <shaunakdpandit@gmail.com>
2026-01-07 00:07:55 -0500

    none of the binds work
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/programs/browsers/qutebrowser.nix
+++ home/programs/browsers/qutebrowser.nix
@@ -170,9 +170,6 @@ in {
         " w" = "tab-close";
         "J" = "tab-prev";
         "K" = "tab-next";
-        "  " = "tab-select ";
-        " sb" = "quickmark-load ";
-        " /" = "search";
 
         " 1" = "tab-focus 1";
         " 2" = "tab-focus 2";
