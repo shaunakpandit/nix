1cb5f9523fba25007173b14361843b5925210d21
shaunakpandit <shaunakdpandit@gmail.com>
2026-01-12 17:31:39 -0500

    upgrade flake and port of changes from haid
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/system/hyprland/bindings.nix
+++ home/system/hyprland/bindings.nix
@@ -6,7 +6,7 @@
     bind =
       [
         "$mod,RETURN, exec, uwsm app -- ${pkgs.ghostty}/bin/ghostty" # Ghostty (terminal)
-        "$mod,E, exec,  uwsm app -- ${pkgs.xfce.thunar}/bin/thunar" # Thunar
+        "$mod,E, exec,  uwsm app -- ${pkgs.thunar}/bin/thunar" # Thunar
         "$mod,B, exec,  uwsm app -- ${pkgs.qutebrowser}/bin/qutebrowser" # Browser (Qute)
         "$mod,P, exec,  uwsm app -- ${pkgs.proton-pass}/bin/proton-pass" # Proton Pass
         "$mod,V, exec,  uwsm app -- ${pkgs.protonvpn-gui}/bin/protonvpn-app" # Proton VPN
