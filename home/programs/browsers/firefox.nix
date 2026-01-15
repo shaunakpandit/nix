8d59adb165914c5d18e9f5e3c631a24e7071a69d
shaunakpandit <shaunakdpandit@gmail.com>
2026-01-14 22:43:04 -0500

    fuxk fine
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -1,19 +1,9 @@
-{pkgs, lib, inputs, ...}:
-  let
-    lock-false = {
-      Value = false;
-      Status = "locked";
-    };
-    lock-true = {
-      Value = true;
-      Status = "locked";
-    };
-  in
-{
+{pkgs, lib, inputs, ...}: {
   stylix.targets.firefox = {
     enable = true;
     # profileNames = ["ploopy"];
   };
+  
   programs.firefox = {
       enable = true;
       profiles = {
@@ -34,53 +24,77 @@
             # for stylix to work
             "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
             "layout.css.prefers-color-scheme.content-override" = 0;
-          };
-          /* ---- POLICIES ---- */
-          # Check about:policies#documentation for options.
-          policies = {
-            DisableTelemetry = true;
-            DisableFirefoxStudies = true;
-            EnableTrackingProtection = {
-              Value= true;
-              Locked = true;
-              Cryptomining = true;
-              Fingerprinting = true;
-            };
-            DisablePocket = true;
-            DisableFirefoxAccounts = true;
-            DisableAccounts = true;
-            DisableFirefoxScreenshots = true;
-            OverrideFirstRunPage = "";
-            OverridePostUpdatePage = "";
-            DontCheckDefaultBrowser = true;
-            DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
-            DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
-            SearchBar = "unified"; # alternative: "separate"
+            # "browser.startup.homepage" = "https://searx.aicampground.com";
+            "browser.search.defaultenginename" = "google";
+            "browser.search.order.1" = "google";
+            # Disable irritating first-run stuff
+            "browser.disableResetPrompt" = true;
+            "browser.download.panel.shown" = true;
+            "browser.feeds.showFirstRunUI" = false;
+            "browser.messaging-system.whatsNewPanel.enabled" = false;
+            "browser.rights.3.shown" = true;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.shell.defaultBrowserCheckCount" = 1;
+            "browser.startup.homepage_override.mstone" = "ignore";
+            "browser.uitour.enabled" = false;
+            "startup.homepage_override_url" = "";
+            "trailhead.firstrun.didSeeAboutWelcome" = true;
+            "browser.bookmarks.restore_default_bookmarks" = false;
+            "browser.bookmarks.addedImportButton" = true;
 
-            /* ---- PREFERENCES ---- */
-            # Check about:config for options.
-            Preferences = { 
-              "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-              "extensions.pocket.enabled" = lock-false;
-              "extensions.screenshots.disabled" = lock-true;
-              "browser.topsites.contile.enabled" = lock-false;
-              "browser.formfill.enable" = lock-false;
-              "browser.search.suggest.enabled" = lock-false;
-              "browser.search.suggest.enabled.private" = lock-false;
-              "browser.urlbar.suggest.searches" = lock-false;
-              "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
-              "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-              "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-              "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-              "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-              "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-              "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-              "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-              "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-              "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-            };
-          };
+            # Don't ask for download dir
+            "browser.download.useDownloadDir" = false;
 
+            # Disable crappy home activity stream page
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+            "browser.newtabpage.blocked" = lib.genAttrs [
+              # Youtube
+              "26UbzFJ7qT9/4DhodHKA1Q=="
+              # Facebook
+              "4gPpjkxgZzXPVtuEoAL9Ig=="
+              # Wikipedia
+              "eV8/WsSLxHadrTL1gAxhug=="
+              # Reddit
+              "gLv0ja2RYVgxKdp0I5qwvA=="
+              # Amazon
+              "K00ILysCaEq8+bEqV/3nuw=="
+              # Twitter
+              "T9nJot5PurhJSy8n038xGA=="
+            ] (_: 1);
+
+            # Disable some telemetry
+            "app.shield.optoutstudies.enabled" = false;
+            "browser.discovery.enabled" = false;
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
+            "browser.ping-centre.telemetry" = false;
+            "datareporting.healthreport.service.enabled" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "datareporting.policy.dataSubmissionEnabled" = false;
+            "datareporting.sessions.current.clean" = true;
+            "devtools.onboarding.telemetry.logged" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.hybridContent.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.prompted" = 2;
+            "toolkit.telemetry.rejected" = true;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "toolkit.telemetry.server" = "";
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.unifiedIsOptIn" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
+
+            # Disable "save password" prompt
+            "signon.rememberSignons" = false;
+            # Harden
+            "privacy.trackingprotection.enabled" = true;
+            "dom.security.https_only_mode" = true;
+          };
           search = {
             force = true;
             default = "google";
