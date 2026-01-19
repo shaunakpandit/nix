3b5a6c2678c4e551a9949d76457524f4688e4d1d
shaunakpandit <shaunakdpandit@gmail.com>
2026-01-19 16:03:01 -0500

    minor theme changes - increas font size, adjust opacity and border size
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- themes/darkmetal.nix
+++ themes/darkmetal.nix
@@ -5,10 +5,10 @@
       rounding = 0;
       gaps-in = 5;
       gaps-out = 5 * 2;
-      active-opacity = 0.96;
-      inactive-opacity = 0.92;
+      active-opacity = 0.94;
+      inactive-opacity = 0.88;
       blur = true;
-      border-size = 1;
+      border-size = 2;
       animation-speed = "fast"; # "fast" | "medium" | "slow"
       fetch = "nerdfetch"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
       textColorOnWallpaper =
@@ -22,28 +22,28 @@
 
     # See https://tinted-theming.github.io/tinted-gallery/ for more schemes
     base16Scheme = {
-      base00= "#000000";
-      base01= "#121212";
-      base02= "#222222";
-      base03= "#333333";
-      base04= "#999999";
-      base05= "#c1c1c1";
-      base06= "#999999";
-      base07= "#c1c1c1";
-      base08= "#5f8787";
-      base09= "#aaaaaa";
-      base0A= "#556677";
-      base0B= "#7799bb";
-      base0C= "#aaaaaa";
-      base0D= "#888888";
-      base0E= "#b87ab8";
-      base0F= "#444444";
+      base00 = "#000000";
+      base01 = "#121212";
+      base02 = "#222222";
+      base03 = "#333333";
+      base04 = "#999999";
+      base05 = "#c1c1c1";
+      base06 = "#999999";
+      base07 = "#c1c1c1";
+      base08 = "#5f8787";
+      base09 = "#aaaaaa";
+      base0A = "#556677";
+      base0B = "#7799bb";
+      base0C = "#aaaaaa";
+      base0D = "#888888";
+      base0E = "#b87ab8";
+      base0F = "#444444";
     };
 
     cursor = {
       name = "phinger-cursors-light";
       package = pkgs.phinger-cursors;
-      size = 24;
+      size = 32;
     };
 
     fonts = {
@@ -61,10 +61,10 @@
         name = "Noto Color Emoji";
       };
       sizes = {
-        applications = 15;
-        desktop = 15;
-        popups = 15;
-        terminal = 15;
+        applications = 18;
+        desktop = 18;
+        popups = 18;
+        terminal = 18;
       };
     };
 
@@ -76,10 +76,15 @@
       # singularity
       # url = "https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/planet_minimal.png";
       # sha256 = "sha256-dUSrAjvFzkXr2Xb5/kPkS5fwB66CycPrB8CqcFGCycM=";
-
       # mountains and stars
-      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/moutains-and-stars_black.png";
-      sha256 = "sha256-xVhaeAGs4U1zpx/WlGv17Vtr3uw92qNq0JvgXqAnEpE=";
+      # url =
+      #   "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/moutains-and-stars_black.png";
+      # sha256 = "sha256-xVhaeAGs4U1zpx/WlGv17Vtr3uw92qNq0JvgXqAnEpE=";
+
+      # svdden death 
+      url =
+        "https://raw.githubusercontent.com/shaunakpandit/wallpapers/main/Desktop/SvddenDeathBackgroundSkinny.png";
+      sha256 = "sha256-PCiiNuRtNaVDJBZFh0znxUbADX/Wd3j0+xOnkoiWXNw=";
     };
   };
 }
