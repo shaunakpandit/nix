5ef3099380ded2bebd08479df438e1f356879805
shaunakpandit <shaunakdpandit@gmail.com>
2026-03-24 21:50:46 -0400

    configure
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/programs/neovim/nvim/lua/plugins/iwe.lua
+++ home/programs/neovim/nvim/lua/plugins/iwe.lua
@@ -1,8 +1,11 @@
 vim.pack.add({
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 	{ src = "https://github.com/iwe-org/iwe.nvim" },
-	{ src = "https://github.com/ibhagwan/fzf-lua" },
 })
 
 -- src: https://github.com/iwe-org/iwe.nvim
-require("iwe").setup({})
+require("iwe").setup({
+    picker = {
+        backend = "vim_ui"
+    }
+})
