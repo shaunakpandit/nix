9b00c80d40fc1e26bf1ed70c683125c08c6752e2
shaunakpandit <shaunakdpandit@gmail.com>
2026-04-12 19:31:23 -0400

    fix hyprland with new update
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- home/system/hyprland/default.nix
+++ home/system/hyprland/default.nix
@@ -232,7 +232,7 @@ in
       ];
 
       misc = {
-        vfr = true;
+        # vfr = true;
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         disable_autoreload = true;
